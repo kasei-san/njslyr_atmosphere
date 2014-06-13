cf04c7975004be266b327cf53a19a5501d3a8211
kasei_san <kasei.san@gmail.com>
2014-06-13 12:37:58 +0900

    Add #beforePdf for replace imagetag

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,6 @@
 var gulp        = require('gulp');
+var tap         = require('gulp-tap');
+var path        = require('path');
 var markdown    = require('gulp-markdown');
 var markdownpdf = require('gulp-markdown-pdf');
 var clean       = require('gulp-clean');
@@ -8,14 +10,40 @@ gulp.task('default', ['clean', 'html', 'pdf']);
 
 // markdown to html
 gulp.task('html', function () {
-  return gulp.src('markdown/*.md')
+  return gulp.src('dist/markdown/*.md')
     .pipe(markdown())
     .pipe(gulp.dest('dist/html'));
 });
 
+// before markdown to pdf
+gulp.task('beforePdf', function () {
+
+  function cd(){
+    var fs = require('fs');
+    return fs.realpathSync('./');
+  }
+
+  function replaceImgTag(file) {
+    // replace img tag
+    var filename = path.basename(file.path);
+    var imgdir = path.join(cd(), "img", filename.replace(".md", ''))
+    file.contents = new Buffer(
+      String(file.contents).replace(/\b.*jpg/, imgdir + "/$&")
+    );
+  }
+
+  return gulp.src('markdown/*.md')
+    .pipe(
+      tap(function (file) {
+        replaceImgTag(file);
+      })
+    )
+    .pipe(gulp.dest('dist/markdown/pdf/'));
+});
+
 // markdown to pdf
 gulp.task('pdf', function () {
-  return gulp.src('markdown/*.md')
+  return gulp.src('dist/markdown/pdf/*.md')
     .pipe(markdownpdf())
     .pipe(gulp.dest('dist/pdf'));
 });
--- markdown/01_busido_coffee.md
+++ markdown/01_busido_coffee.md
@@ -3,7 +3,7 @@
 ロシアのインスタントコーヒーらしいです
 オフィシャルサイトのいい加減な日本語がじわじわきます
 
-<img src="http://25.media.tumblr.com/tumblr_mddl62rTv51qz56lho1_500.jpg">
+![武士道コーヒー](busido_coffee.jpg)
 
 - [japan-page | Bushido](https://www.bushido-coffee.com/en/node/7)
 
